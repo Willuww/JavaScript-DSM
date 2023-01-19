525ddb5ac2606a564ee8a7df7d8f542ff35b7d75
Willuww <joelbelloromero@gmail.com>
2023-01-19 14:18:06 -0600

    Commit de tema de funciones terminado

--- script.js
+++ script.js
@@ -1,25 +1,95 @@
-function saludo(){
-    console.log("Hola Mundo");
+function hello3(name){
+    return 'Hola' + name;
+}
+console.log (hello3('Luis'));
+console.log(hello3());
+
+function agregar(x,y){
+    return x+y;
+}
+console.log(agregar());
+console.log(agregar(5,4));
+//Parametros por default
+
+function agregar2(x,y){
+    // == dos igual son "igual que"
+    // === tres igual son "exactamente igual"
+    if(y === undefined){
+        y=0;
+    }else if(x === undefined){
+        x=0;
+    }else if (x === undefined && y === undefined)
+    {
+        return 0;
+    }
+ 
+    return x+y;
 }
 
-saludo();
-
-function saludo2(){
-    return'Hola Mundo';
+function agregar3(x, y =1, z=2)
+{
+    return x +y *z;
 }
+console.log(agregar3(5));
 
-const result = saludo2();
+//funciones de flecha
+const suma = () => 3 + 4;
+/* Es lo mimso que esto:
+functions sumar() { return 3+4 }*/ 
 
-console.log(result);
+function sumar() { return 3+4 }
 
-console.log(saludo2());
+console.log(sumar());
+console.log(sumar());
 
-function saludo3(){
+function f1(x){
+    return "Hola soy " + x;
+}
+const f1a = x => "Hola soy " + x;
 
-    return function(){
-        return 'Hola 3';
+console.log(f1("Joel"));
+console.log(f1a("Willuww"));
+
+function factorial(n){
+    if(n == 0 || n==1){
+        return 1;
+    }else {
+        return n*factorial (n-1);
     }
 }
+/*
+const fact = n => {
+    if(n ==0 || n==1){
+        return 1;
+    }else{
+        return n*fact(n-1);
+    }
+}*/
 
-console.log(saludo3());
-console.log(saludo3()());
+function factorial2 (n){
+    i =1;
+    fact = 1;
+    while(i<n){
+        fact*=i;
+        i++;
+    }
+    return fact;
+}
+
+const f= n => {
+    i=1;
+    fact =1;
+    while(i<n){
+        fact*=i;
+        i++;}
+        return fact;
+}
+
+//variacion de if else
+const valor = numero => (numero%2 ==0)?'Numero Par':'Numero Impar'
+
+console.log(factorial(3));
+console.log(factorial2(3));
+/*console.log(fact(3));*/
+console.log(f(3));
+console.log(valor(2));
